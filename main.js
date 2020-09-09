4fcd01c2996b043780ca1ab588bdedfeed725c04
Vlasya <64101971+Vlasya@users.noreply.github.com>
2020-09-09 23:52:00 +0300

    1

--- main.js
+++ main.js
@@ -1,35 +1,12 @@
-// We have an array of arrays. Each array represents a pet. The pets have a name, an age, and a type.
-// let pets = [
-//   ['Butters', 3, 'dog'],
-//   ['Lizzy', 6, 'dog'],
-//   ['Red', 1, 'cat'],
-//   ['Joey', 3, 'dog'],
-// ];
-// Task:
-// Select only the dogs
-// Translate their ages into dog years (multiply them by seven)
-// Sum the results
+// Count the frequency of each character in the string 'helloworld'
+let string='helloworld'
+
+let character =string.split('');
+console.log('character: ', character);
+
+function count (item){
+	var count=0
+	for
+}
 
 
-let pets = [
-	
-	['Butters', 3, 'dog'],
-	['Lizzy', 6, 'dog'],
-	['Red', 1, 'cat'],
-	['Joey', 3, 'dog'],
- ];
-// Select only the dogs
-let myPets= pets.filter(item=>item[2]==='dog')
-// Translate their ages into dog years (multiply them by seven)
- 						.map(el =>{
-							el[1]=el[1]*7
-							return el
-						 }) 
-//  Sum the results
-						.reduce((summ,item) =>{
-							summ+=item[1]
-							return summ
-						},0)
-
-
- console.log('Сумма псиных годиков : ', myPets);
